f01b07981908b7aed25e66c45b8e6c9ace1b982c
Rafael Avelar Pacheco <rafaelpa@takenet.com.br>
2016-11-03 19:26:24 -0200

    add a resume webhook bot sample

---
+++ samples/webhook/ResumeBot/ResumeBot/Controllers/NotificationsController.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ResumeBot.Controllers
+{
+    public class NotificationsController : ApiController
+    {
+        // POST api/values
+        public void Post([FromBody]JObject jsonObject)
+        {
+            Console.WriteLine($"Received Notification  {jsonObject}");
+        }
+    }
+}
---
+++ samples/webhook/ResumeBot/ResumeBot/Services/HttpRetryHandler.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace ResumeBot.Services
+{
+    public class HttpRetryHandler : DelegatingHandler
+    {
+        // Strongly consider limiting the number of retries - "retry forever" is
+        // probably not the most user friendly way you could respond to "the
+        // network cable got pulled out."
+        private readonly int MaxRetries;
+
+        public HttpRetryHandler(HttpMessageHandler innerHandler, int maxRetries)
+            : base(innerHandler)
+        {
+            MaxRetries = maxRetries;
+        }
+
+        protected override async Task<HttpResponseMessage> SendAsync(
+            HttpRequestMessage request,
+            CancellationToken cancellationToken)
+        {
+            HttpResponseMessage response = null;
+            for (var i = 0; i < MaxRetries; i++)
+            {
+                response = await base.SendAsync(request, cancellationToken);
+                if (response.IsSuccessStatusCode ||
+                    IsClientFailure(response.StatusCode) ||
+                    cancellationToken.IsCancellationRequested)
+                {
+                    return response;
+                }
+                await Task.Delay(TimeSpan.FromSeconds(Math.Pow(2, i)), cancellationToken);
+            }
+
+            return response;
+        }
+
+        private bool IsClientFailure(HttpStatusCode statusCode)
+        {
+            var statusCodeValue = (int)statusCode;
+            return statusCodeValue >= 400 && statusCodeValue < 500;
+        }
+    }
+}
